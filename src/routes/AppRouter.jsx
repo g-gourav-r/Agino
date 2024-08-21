7c8b4f4917651c729d0a17c44d66fc973177d1f7
g-gourav-r <gouravrchandra@gmail.com>
2024-08-21 21:30:54 +0530

    feature | Add Signup page

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import AginoPage from '../pages/AginoPage';
 import LoginPage from '../pages/LoginPage';
+import SignupPage from '../pages/SignupPage';
 
 function AppRouter() {
     return (
@@ -10,6 +11,7 @@ function AppRouter() {
         <Routes>
             <Route path="/" element={<AginoPage />} />
             <Route path="/user/login" element={<LoginPage />} />
+            <Route path="/user/signup" element={<SignupPage />} />
         </Routes>
       </Router>
     );
