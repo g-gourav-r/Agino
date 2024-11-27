88f167967e96caf12d248da16c582c4ba4dc2c90
g-gourav-r <gouravrchandra@gmail.com>
2024-11-28 01:04:30 +0530

    test

--- src/components/HomePage/HomePageMainContent.jsx
+++ src/components/HomePage/HomePageMainContent.jsx
@@ -5,7 +5,7 @@ function HomePageMainContent() {
     <div className="d-flex align-items-center justify-content-center h-100">
       <div className="container d-flex  flex-column align-items-center justify-content-center h-100">
         <h1>
-          Hello <span className="text-green">User</span> !!!!
+          Hello <span className="text-green">User</span> !!!
         </h1>
         <h3>
           Welcome to <span className="text-green">Agino</span>
